baf22da25d0d8e2b7871710f0f06954e7e51588a
SamuelGarcia22 <101756868+SamuelGarcia22@users.noreply.github.com>
2022-04-05 09:37:50 -0500

    Code improved

--- mails.py
+++ mails.py
@@ -5,18 +5,18 @@ from email.mime.multipart import MIMEMultipart
 from jinja2 import Environment, FileSystemLoader
 import os
 
-from data import data_file
-
+from data_errormail import data_file_filled, data_file_partial, data_file_empty
+from globals import g_to_email, g_from_email, g_password_email
 
 env = Environment(loader=FileSystemLoader('%s/templates/' % os.path.dirname(__file__)))
 
 def get_data():
-    data = data_file
+    data = data_file_partial
     return data
 
 def send_mail(bodyContent):
-    to_email = ''
-    from_email = ''
+    to_email = g_to_email
+    from_email = g_from_email
     subject = 'CFDIS con errores reporte mensual'
     message = MIMEMultipart()
     message['Subject'] = subject
@@ -28,14 +28,14 @@ def send_mail(bodyContent):
 
     server = SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(from_email, '')
+    server.login(from_email, g_password_email)
     server.sendmail(from_email, to_email, msgBody)
     server.quit()
 
 def send_movie_list():
     json_data = get_data()
-    template = env.get_template('child.html')
-    bodyContent = template.render(data=json_data)
+    template = env.get_template('cfdi-mail-notifications.html')
+    bodyContent = template.render(json_data)
     send_mail(bodyContent)
     return "Mail sent successfully"
 
