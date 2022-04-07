cc9a9c82ef39b9c691a771fd7b8fe1069a65c2e1
SamuelGarcia22 <101756868+SamuelGarcia22@users.noreply.github.com>
2022-04-06 19:50:24 -0500

    Imported some data files

--- mails.py
+++ mails.py
@@ -6,18 +6,21 @@ from jinja2 import Environment, FileSystemLoader
 import os
 
 from data_errormail import data_file_filled, data_file_partial, data_file_empty
+from data_efosmail import efos_data_file_filled
+from data_confirmmail import confirm_data
+from data_temporarypass import data_pass
 from globals import g_to_email, g_from_email, g_password_email
 
 env = Environment(loader=FileSystemLoader('%s/templates/' % os.path.dirname(__file__)))
 
 def get_data():
-    data = data_file_filled
+    data = data_pass
     return data
 
 def send_mail(bodyContent):
     to_email = g_to_email
     from_email = g_from_email
-    subject = 'CFDIS con errores [Reporte mensual]'
+    subject = 'Contraseña temporal'
     message = MIMEMultipart()
     message['Subject'] = subject
     message['From'] = from_email
@@ -34,7 +37,7 @@ def send_mail(bodyContent):
 
 def send_movie_list():
     json_data = get_data()
-    template = env.get_template('cfdi-mail-notifications.html')
+    template = env.get_template('solcp-temporary-password.html')
     bodyContent = template.render(json_data)
     send_mail(bodyContent)
     return "Mail sent successfully"
